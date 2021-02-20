98c9788e53531a12d563400b43ffd9f808953a52
Seth Whitaker <sethjwhitaker@yahoo.com>
2021-02-20 03:40:58 -0800

    Enable MySQL connection in Express server
    Add test model, controller, and router, add test db_init, add db setup db.js, allow use of ES6 classes with babel

---
+++ server/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const dbConfig = require("./config/db.config.js");
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  database: dbConfig.DB
+});
+
+// open the MySQL connection
+connection.connect(error => {
+  if (error) console.log(error) 
+  else 
+    console.log("Successfully connected to the database.");
+});
+
+module.exports = connection;
