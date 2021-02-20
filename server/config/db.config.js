a051ebd482fa5aa937e40ea22404046d502f2b76
Seth Whitaker <sethjwhitaker@yahoo.com>
2021-02-20 05:02:52 -0800

    Config for deploying

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    DB: 'webapptest'
+    HOST: "us-cdbr-east-03.cleardb.com",
+    USER: "b89c47ec0a2650",
+    PASSWORD: "4034407c",
+    DB: "heroku_52c9314728ab064"
 }
--- server/db.js
+++ server/db.js
@@ -2,9 +2,10 @@ const mysql = require("mysql");
 const dbConfig = require("./config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
   database: dbConfig.DB
 });
 
