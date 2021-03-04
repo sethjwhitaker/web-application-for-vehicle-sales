c13ab6bc4f7c2c4c27c36bb5a8f2b970e1d2cac5
BradyNeumann <78769834+BradyNeumann@users.noreply.github.com>
2021-03-04 04:55:19 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ class App extends Component {
   render(){
     return(
         <div>
-
-            <div class="jumbotron">
-                <Navbar />
-            </div>
+          <Navbar />
         
         </div>
     );
