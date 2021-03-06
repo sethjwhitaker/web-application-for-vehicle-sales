678e2023a5a9766e2b4db0ae2681bac258aa1293
Taylor Cook <tecook2@mail.fhsu.edu>
2021-03-05 18:58:09 -0600

    Added Login and Register Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
+import { useState } from "react";
 import {hot} from "react-hot-loader";
 import Button from 'react-bootstrap/Button';
+import Register from './Register.js';
+import Login from './Login.js';
+import { Form } from "react-bootstrap";
+
 
 class App extends Component {
   render(){
     return(
         <div>
 
-            <div class="jumbotron">
-                <h1 class="display-4">Hello World</h1>
-                <p class="lead">Team Project</p>
+            <div className="jumbotron">
+                <h1 className="display-4">Hello World</h1>
+                <p className="lead">Team Project</p>
                 <Button>Example Button</Button>
             </div>
         
+            <Login />
         </div>
     );
   }
---
+++ src/Register.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+
+    return (
+    <div className="Register">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block type="submit" disabled={!validateForm()}>
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
+}
