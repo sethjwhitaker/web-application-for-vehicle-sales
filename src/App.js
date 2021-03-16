c9d1d8d0f129542097b5c0673409296b3b84cd36
Fatih Gurbuz <timsecsectim@gmail.com>
2021-03-16 18:48:30 +0300

    Created home page and added required components and pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import React, { Component } from "react";
-import { useState } from "react";
+import React, { Component} from "react";
 import {hot} from "react-hot-loader";
-import Button from 'react-bootstrap/Button';
-import Register from './Register.js';
-import Login from './Login.js';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import SearchBar from './components/SearchBar/SearchBar';
+import Main from './components/Main/Main';
 import Header from './components/Header';
-import { Form } from "react-bootstrap";
+import axios from "axios"
 
 class App extends Component {
+  
+  state = {
+    cars:[],
+    searchedProducts:[],
+    carBrands:[]
+    
+  }
+ 
+  componentDidMount(){
+     axios.get('http://localhost:8000/cars').then(response =>{
+       this.setState({cars:response.data, searchedProducts:response.data})
+     console.log("Data Loaded!")
+     console.log(response.data);
+     }).catch(err => {
+       console.error("Request Not Found!");
+     })
+     
+  }
+
+
+  //Searching -> Searchbar
+  searched = (inputValue) => {
+    let searchedProducts = this.state.cars.filter((product) =>
+      product.brand
+        .toLowerCase()
+        .includes(inputValue.toLowerCase().trim())
+    );
+    this.setState({ searchedProducts: searchedProducts });
+
+  }
+
+  //Filtering by brand -> Sidebar
+  optionSelected = (selectedBrand) => {
+    let carBrand = this.state.cars.filter((car) => 
+      car.brand.toLowerCase()
+      .includes(selectedBrand.toLowerCase())
+    )
+    this.setState({ searchedProducts: carBrand });
+    this.setState({carBrands: carBrand})
+
+  }
+
+  modelSelected = (models)=> {
+    axios.get(`http://localhost:8000/cars?model=${models}`).
+    then(response => this.setState({searchedProducts:response.data}))
+  } 
+
+
+  //Rendering
   render(){
-    return(
-        <div className="jumbotron">
-            <div>
-                <Navbar />
-                <Header />
-            </div>
-        </div>
+    return(      
+      <div>
+          <div>
+                 <Navbar />
+                 <Header />
+                 <SearchBar searched={this.searched}/>
+                 <Main modelSelected={this.modelSelected} carBrands={this.state.carBrands} optionSelected={this.optionSelected} dataCar={this.state.cars}data={this.state.searchedProducts}/>
+                 <Footer />
+            </div>  
+      </div>
+      
     );
   }
 }
