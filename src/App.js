d8a544e87aa44c5f735d8cceaf7fabd41e471148
furbuz <49695019+furbuz@users.noreply.github.com>
2021-03-24 00:15:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import Admin_Interface from './components/Admin/Admin_Interface.js';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import SearchBar from './components/SearchBar/SearchBar';
+import Main from './components/Main/Main';
+import Header from './components/Header';
+import axios from "axios"
 
 class App extends Component {
   
+  state = {
+    cars:[],
+    searchedProducts:[],
+    carBrands:[]
+    
+  }
+ 
+  componentDidMount(){
+     axios.get('http://localhost:8000/cars').then(response =>{
+       this.setState({cars:response.data, searchedProducts:response.data})
+     console.log("Data Loaded!")
+     console.log(response.data);
+     }).catch(err => {
+       console.error("Request Not Found!");
+     })
+     
+  }
+
+
+  //Searching -> Searchbar
+  searched = (inputValue) => {
+    let searchedProducts = this.state.cars.filter((product) =>
+      product.brand
+        .toLowerCase()
+        .includes(inputValue.toLowerCase().trim())
+    );
+    this.setState({ searchedProducts: searchedProducts });
+
+  }
+
+  //Filtering by brand -> Sidebar
+  optionSelected = (selectedBrand) => {
+    let carBrand = this.state.cars.filter((car) => 
+      car.brand.toLowerCase()
+      .includes(selectedBrand.toLowerCase())
+    )
+    this.setState({ searchedProducts: carBrand });
+    this.setState({carBrands: carBrand})
+
+  }
+
+  modelSelected = (models)=> {
+    axios.get(`http://localhost:8000/cars?model=${models}`).
+    then(response => this.setState({searchedProducts:response.data}))
+  } 
+
 
   //Rendering
   render(){
     return(      
       <div>
-          
-         <Admin_Interface />
-            
+          <div>
+                 <Navbar />
+                 <Header />
+                 <SearchBar searched={this.searched}/>
+                 <Main modelSelected={this.modelSelected} carBrands={this.state.carBrands} optionSelected={this.optionSelected} dataCar={this.state.cars}data={this.state.searchedProducts}/>
+                 <Admin_Interface />
+                 <Footer />
+            </div>  
       </div>
       
-
     );
   }
 }
-
 
 export default hot(module)(App);
