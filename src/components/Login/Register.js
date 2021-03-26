bf570d31cf431c33e67cd0ed1415b2718cba070b
Seth Whitaker <sethjwhitaker@yahoo.com>
2021-03-25 22:09:27 -0700

    Remove postcss import

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import styles from "./login.css";
-import { stringify } from "postcss";
 
 export default function Register() {
     const [newEmail, setEmail] = useState("");
