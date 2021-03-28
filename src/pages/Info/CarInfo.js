4e8e6040a3fa0cae94a85769b1faa892f11f0701
BradyNeumann <bneumann97@gmail.com>
2021-03-28 07:53:49 -0500

    Testing withRouter
    Attempting to use withRouter to get vehicle id for fetch

--- src/pages/Info/CarInfo.js
+++ src/pages/Info/CarInfo.js
@@ -1,62 +1,61 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import {useParams} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import Email from '../../components/Info/Email';
 import Description from '../../components/Info/Description';
 import ProductHeader from '../../components/Info/ProductHeader';
 import ProductInfo from '../../components/Info/ProductInfo';
 import './CarInfo.css';
-import { data } from 'autoprefixer';
 
 
-export default function CarInfo () {
-
-    
-    
-    let params = useParams();
-    
-    fetch(`${window.location.protocol}//${window.location.hostname}/vehicles/${params.id}`, {
-        headers: {
-            "Content-type": "application/json"
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("data is:");
-        console.log(data);
+class CarInfo extends Component {
+    render() {
         
-    })
-
-
-    return (
-    <div>
-        <Container className="flex-container">
-            
-            <ProductHeader
-            title_header = "Vehicle Title"
-            mileage_header = "Mileage"
-            price_header = "Price"
-            title = "?"
-            mileage = "?"
-            price = {this.data.price}
-            />
-            
-            <Row>
-                <ProductInfo
-                imgURL = "https://www.linkpicture.com/q/LPic604691ad669e8533277294.jpg"
-                prod = "vehicle"
-
+        componentDidMount() 
+        {
+            fetch(`${window.location.protocol}//${window.location.hostname}/vehicles/${this.props.match.params.id}`, {
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("data is:");
+                console.log(data);
+            })
+        }
+    
+        return (
+        <div>
+            <Container className="flex-container">
+                
+                <ProductHeader
+                title_header = "Vehicle Title"
+                mileage_header = "Mileage"
+                price_header = "Price"
+                title = "?"
+                mileage = "?"
+                price = "?"
                 />
-
-                <Col className="col-6">
-                    <Description 
-                    desc = "A text description about the vehicle here with everything a customer will need to know."
+                
+                <Row>
+                    <ProductInfo
+                    imgURL = "https://www.linkpicture.com/q/LPic604691ad669e8533277294.jpg"
+                    prod = "vehicle"
+    
                     />
-                    <Email />
-
-                </Col>
-            </Row>
-        </Container>
-    </div>
-    )
+    
+                    <Col className="col-6">
+                        <Description 
+                        desc = "A text description about the vehicle here with everything a customer will need to know."
+                        />
+                        <Email />
+    
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+        )
+    }
 }
+export default withRouter(CarInfo)
