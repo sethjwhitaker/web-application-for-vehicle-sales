7ea230863168b76c95cdb4146151b7930f789a60
Taylor Cook <tecook2@mail.fhsu.edu>
2021-04-11 11:27:46 -0500

    Attempt at mapping over array to display table

--- src/components/Admin/Classes.js
+++ src/components/Admin/Classes.js
@@ -10,18 +10,13 @@ class Classes extends React.Component {
     }
 
     async componentDidMount() {
-        const url = "https://api.randomuser.me/";
         const response =  await fetch(`${window.location.protocol}//${window.location.hostname}/makes`, {
-
             headers: {
             "Content-type": "application/json"
             }
         });
         const data = await response.json();
         this.setState({makes: data, loading: false})
-        console.log(data[0].id);
-        console.log(data[0].name);
-        
     }
 
     render() {
@@ -34,11 +29,36 @@ class Classes extends React.Component {
         }
 
         return  (
-            <div>
-                <div>{this.state.makes[0].id}</div>
-                <div>{this.state.makes[0].name}</div>
-                <div>{this.state.makes[1].id}</div>
-                <div>{this.state.makes[1].name}</div>
+            <div className = "container-fluid">
+                <h2>Makes List</h2>
+                <div className="tablediv">
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Make ID</th>
+                                <th>Make Name</th>
+                                <th>Update?</th>
+                                <th>Delete?</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/*loop to display each make on a row*/}
+                            {this.state.makes.map((e) => (
+                                <tr key={e.id}>
+                                    <td>{e.id}</td>
+                                    <td>{e.name}</td>
+                                    <td>-</td>
+                                    <td>-</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    {/*test makes returns*/}
+                    <div>{this.state.makes[0].id}</div>
+                    <div>{this.state.makes[0].name}</div>
+                    <div>{this.state.makes[1].id}</div>
+                    <div>{this.state.makes[1].name}</div>
+                </div>
             </div>
         );
     }
