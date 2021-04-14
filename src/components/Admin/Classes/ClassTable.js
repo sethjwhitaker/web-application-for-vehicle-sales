378ff42e369a6746c440072d7c8f7347e845d4df
Taylor Cook <tecook2@mail.fhsu.edu>
2021-04-13 22:29:50 -0500

    Full functionality for classes/makes/types

--- src/components/Admin/Classes/ClassTable.js
+++ src/components/Admin/Classes/ClassTable.js
@@ -1,14 +1,49 @@
 import React from 'react'
 import 'regenerator-runtime/runtime';
 import Table from 'react-bootstrap/Table'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class ClassTable extends React.Component {
-
-    state = {
-        loading: true,
-        classes: null,
+    constructor() {
+        super();
+        this.state = {
+            loading: true,
+            classes: null,
+            deleted: false,
+            idToDelete: 0,
+          };
+        // Binding method
+        this.onDelete = this.onDelete.bind(this);
     }
 
+    onDelete = (e) => {
+        this.state.idToDelete = e.target.getAttribute('value');
+
+        // POST request using fetch()
+        fetch(`${window.location.protocol}//${window.location.hostname}/classes/${this.state.idToDelete}`, {
+            
+        // Adding method type
+        method: "DELETE",
+            
+        // Adding body or contents to send
+        body: JSON.stringify({
+            id: this.state.idToDelete
+        }),
+            
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+    
+        // Converting to JSON
+        .then(response => response.json())
+    
+        // Displaying results to console
+        .then(json => console.log(json));
+    }
+    
     async componentDidMount() {
         const response =  await fetch(`${window.location.protocol}//${window.location.hostname}/classes`, {
             headers: {
@@ -17,8 +52,8 @@ class ClassTable extends React.Component {
         });
         const data = await response.json();
         this.setState({classes: data, loading: false})
-        console.log(data)
     }
+
 
     render() {
         if (this.state.loading) {
@@ -32,14 +67,14 @@ class ClassTable extends React.Component {
         return  (
             <div className = "container-fluid">
                 <h2>Classes List</h2>
+                <p>Note: Classes cannot be deleted if they exist in either sales history or current inventory.</p>
                 <div className="tablediv">
                     <Table striped bordered hover>
                         <thead>
                             <tr>
                                 <th>Class ID</th>
                                 <th>Class Name</th>
-                                <th>Update?</th>
-                                <th>Delete?</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -48,8 +83,11 @@ class ClassTable extends React.Component {
                                 <tr key={e.id}>
                                     <td>{e.id}</td>
                                     <td>{e.name}</td>
-                                    <td>-</td>
-                                    <td>-</td>
+                                    <td>
+                                        <Button className="" value={e.id} onClick={this.onDelete} block type="submit">
+                                            Delete
+                                        </Button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
--- src/components/Admin/Types/TypeTable.js
+++ src/components/Admin/Types/TypeTable.js
@@ -1,14 +1,49 @@
 import React from 'react'
 import 'regenerator-runtime/runtime';
 import Table from 'react-bootstrap/Table'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class TypeTable extends React.Component {
-
-    state = {
-        loading: true,
-        types: null,
+    constructor() {
+        super();
+        this.state = {
+            loading: true,
+            types: null,
+            deleted: false,
+            idToDelete: 0,
+          };
+        // Binding method
+        this.onDelete = this.onDelete.bind(this);
     }
 
+    onDelete = (e) => {
+        this.state.idToDelete = e.target.getAttribute('value');
+
+        // POST request using fetch()
+        fetch(`${window.location.protocol}//${window.location.hostname}/types/${this.state.idToDelete}`, {
+            
+        // Adding method type
+        method: "DELETE",
+            
+        // Adding body or contents to send
+        body: JSON.stringify({
+            id: this.state.idToDelete
+        }),
+            
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+    
+        // Converting to JSON
+        .then(response => response.json())
+    
+        // Displaying results to console
+        .then(json => console.log(json));
+    }
+    
     async componentDidMount() {
         const response =  await fetch(`${window.location.protocol}//${window.location.hostname}/types`, {
             headers: {
@@ -17,8 +52,8 @@ class TypeTable extends React.Component {
         });
         const data = await response.json();
         this.setState({types: data, loading: false})
-        console.log(data)
     }
+
 
     render() {
         if (this.state.loading) {
@@ -32,24 +67,27 @@ class TypeTable extends React.Component {
         return  (
             <div className = "container-fluid">
                 <h2>Types List</h2>
+                <p>Note: Types cannot be deleted if they exist in either sales history or current inventory.</p>
                 <div className="tablediv">
                     <Table striped bordered hover>
                         <thead>
                             <tr>
                                 <th>Type ID</th>
                                 <th>Type Name</th>
-                                <th>Update?</th>
-                                <th>Delete?</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {/*loop to display each type on a row*/}
+                            {/*loop to display each types on a row*/}
                             {this.state.types.map((e) => (
                                 <tr key={e.id}>
                                     <td>{e.id}</td>
                                     <td>{e.name}</td>
-                                    <td>-</td>
-                                    <td>-</td>
+                                    <td>
+                                        <Button className="" value={e.id} onClick={this.onDelete} block type="submit">
+                                            Delete
+                                        </Button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
