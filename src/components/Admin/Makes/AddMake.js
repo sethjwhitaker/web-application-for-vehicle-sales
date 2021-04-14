f4b334b00cf7a139a9528d49311564ae068d6539
Taylor Cook <tecook2@mail.fhsu.edu>
2021-04-13 22:10:46 -0500

    Deleted some test stuff

--- src/components/Admin/Makes/AddMake.js
+++ src/components/Admin/Makes/AddMake.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Table from 'react-bootstrap/Table';
 
 export default function AddMake() {
     const [newMake, setnewMake] = useState("");
