c3c3bba3415edd2da4f7c3d6285f4e7c47657741
BradyNeumann <bneumann97@gmail.com>
2021-04-19 06:50:29 -0500

    Added Routing
    Added routed to current pages in the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 import * as ReactBootStrap from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return(
-            <ReactBootStrap.Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                <ReactBootStrap.Navbar.Brand href="#home">CSCI 441 Dealership</ReactBootStrap.Navbar.Brand>
+            <ReactBootStrap.Navbar collapseOnSelect expand="md" bg="primary" variant="primary">
+                <ReactBootStrap.Navbar.Brand as={Link} to="/">CSCI 441 Dealership</ReactBootStrap.Navbar.Brand>
                 <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
                     <ReactBootStrap.Nav className="mr-auto">
                     <ReactBootStrap.NavDropdown title="Products" id="collasible-nav-dropdown">
-                        <ReactBootStrap.NavDropdown.Item href="#">Cars</ReactBootStrap.NavDropdown.Item>
+                        <ReactBootStrap.NavDropdown.Item as={NavLink} to="/">Cars</ReactBootStrap.NavDropdown.Item>
                         <ReactBootStrap.NavDropdown.Divider />
-                        <ReactBootStrap.NavDropdown.Item href="#">Parts</ReactBootStrap.NavDropdown.Item>
+                        <ReactBootStrap.NavDropdown.Item as={NavLink} to="#">Parts</ReactBootStrap.NavDropdown.Item>
                     </ReactBootStrap.NavDropdown>
-                    <ReactBootStrap.Nav.Link href="#">Financing</ReactBootStrap.Nav.Link>
+                    <ReactBootStrap.Nav.Link as={NavLink} to="/financing">Financing</ReactBootStrap.Nav.Link>
                     </ReactBootStrap.Nav>
                     <ReactBootStrap.Nav>
                     <ReactBootStrap.NavDropdown title="Account" id="collasible-nav-dropdown">
-                        <ReactBootStrap.NavDropdown.Item href="#">Login</ReactBootStrap.NavDropdown.Item>
+                        <ReactBootStrap.NavDropdown.Item as={NavLink} to="/login">Login</ReactBootStrap.NavDropdown.Item>
                         <ReactBootStrap.NavDropdown.Divider />
-                        <ReactBootStrap.NavDropdown.Item href="#">Register</ReactBootStrap.NavDropdown.Item>
+                        <ReactBootStrap.NavDropdown.Item as={NavLink} to="/register">Register</ReactBootStrap.NavDropdown.Item>
                     </ReactBootStrap.NavDropdown>
-                    <ReactBootStrap.Nav.Link href="#">Cart</ReactBootStrap.Nav.Link>
+                    <ReactBootStrap.Nav.Link as={NavLink} to="#">Cart</ReactBootStrap.Nav.Link>
                     </ReactBootStrap.Nav>
                 </ReactBootStrap.Navbar.Collapse>
             </ReactBootStrap.Navbar>
