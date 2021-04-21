42b0560a875837fa399f832692bf0642682e1c81
Taylor Cook <tecook2@mail.fhsu.edu>
2021-04-20 21:13:30 -0500

    Testing Stuff

--- src/components/Admin/Makes/Makes.js
+++ src/components/Admin/Makes/Makes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AddMake from './AddMake.js';
 import MakeTable from './MakeTable.js';
+import MakesTable from './MakesTable.js';
 
 export default function Makes() {
     return (
         <div>
             <AddMake />
             <MakeTable />
+            <MakesTable />
         </div>
     )
 }
---
+++ src/components/Admin/Makes/MakesTable.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import 'regenerator-runtime/runtime';
+import Table from 'react-bootstrap/Table'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export default function MakesTable() {
+    const url = `${window.location.protocol}//${window.location.hostname}/makes`;
+    const [deleted, setDeleted] = useState(0);
+    const [data, setData] = useState(null);
+    const [editID, setEditID] = useState(null);
+    const [editName, setEditName] = useState(null);
+    const [newEditName, setNewEditName] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [editClicked, setEditClicked] = useState(false);
+
+    useEffect(async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+    }, [deleted]); //only rerender when deleted changes
+    //empty array for onMount only
+
+    //calls delete api with given id
+    const onDelete = (e) => {
+        setDeleted(deleted + 1);
+        console.log(e.target.value);
+    }
+
+    //calls delete api with given id
+    const onEdit = (e) => {
+        console.log(newEditName);
+    }
+
+    const onEditClick = (e) => {
+        setEditID(e.target.getAttribute('first'));
+        setEditName(e.target.getAttribute('last'));
+        setEditClicked(true);
+    }
+
+    return (
+        <div>
+            {/*if loading is true, display loading
+            if editClicked is true, display edit form
+            else display full table*/}
+            {loading ? 
+
+            //render loading
+            <div className = "container-fluid"> ...loading</div> 
+            
+            //if editClicked is true...
+            : editClicked ? 
+
+            //render Edit form
+            <div className = "container-fluid"> 
+                <h2>Edit ID {editID}</h2>
+                <Form onSubmit={(e) => {onEdit(e)}}>
+                    <Form.Group controlId="newEditName">
+                        <Form.Label>New Name</Form.Label>
+                        <Form.Control
+                            type="newEditName"
+                            value={newEditName}
+                            onChange={(e) => setNewEditName(e.target.value)}
+                        />
+                    </Form.Group>
+                    
+                    <Button className="" block type="submit">
+                        Submit
+                    </Button>
+                </Form>   
+                <br></br>
+                <Button className="" onClick={(e) => {setEditClicked(false)}} block type="submit">
+                    Back To Table
+                </Button>
+            </div> 
+
+            //else
+            :
+
+            //display full table
+            <div className = "container-fluid"> 
+                <h2>Makes List</h2>
+                <p>Note: Makes cannot be deleted if they exist in either sales history or current inventory.</p>
+                <div className="tablediv">
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Make ID</th>
+                                <th>Make Name</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/*loop to display each make on a row*/}
+                            {data.map((e) => (
+                                <tr key={e.id}>
+                                    <td>{e.id}</td>
+                                    <td>{e.name}</td>
+                                    <td>
+                                        <Button className="" first={e.id} last={e.name} onClick={(e) => {onEditClick(e)}} block type="submit">
+                                            Edit
+                                        </Button>
+                                    </td>
+                                    <td>
+                                        <Button className="" value={e.id} onClick={(e) => {onDelete(e)}} block type="submit">
+                                            Delete
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))} 
+                        </tbody>
+                    </Table>
+                </div>
+            </div>}
+        </div>
+    )
+}
