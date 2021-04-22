add8200e77ae4205161b4afb2afb4def295fa65a
Taylor Cook <tecook2@mail.fhsu.edu>
2021-04-21 21:47:00 -0500

    Update AddNewCar.js

--- src/components/Admin/AddNewCar.js
+++ src/components/Admin/AddNewCar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
@@ -27,7 +27,9 @@ export default function AddNewCar() {
     const [newMilage, setnewMilage] = useState("");
     const [newShortDescription, setnewShortDescription] = useState("");
     const [newDescription, setnewDescription] = useState("");
-    const [newImage, setnewImage] = useState("");
+    const [makes, setMakes] = useState(null);
+    const [makesLoading, setMakesLoading] = useState(true);
+
     
     //only allows for button press once all required fields are filled out
     function validateForm() {
@@ -35,8 +37,13 @@ export default function AddNewCar() {
         && newPrice.length > 0 && newExtColor.length > 0;
     }
 
-    
-    //**************haven't tested the post request yet********************
+    //import makes
+    useEffect(async () => {
+        const response = await fetch(`${window.location.protocol}//${window.location.hostname}/makes`);
+        const data = await response.json();
+        setMakes(data);
+        setMakesLoading(false);
+    }, []);
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -79,142 +86,151 @@ export default function AddNewCar() {
 
     return (
         <div className="container">
-            {/*Section for adding new cars*/}
+            {/*if loading is true, display loading
+            if editClicked is true, display edit form
+            else display full table*/}
+            {makesLoading ? 
 
-            <h2>Add New Vehicle</h2>
-            <div className="VehicleForm">
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group controlId="make">
-                    <Form.Label>Make</Form.Label>
-                    <Form.Control
-                        type="make"
-                        value={make}
-                        onChange={(e) => setmake(e.target.value)}
-                    />
-                    </Form.Group>
+                //render loading
+                <div className = "container-fluid"> ...loading</div>
+                
+                //else
+                :
+            
+                <div>
 
-                    <Form.Group controlId="type">
-                    <Form.Label>Type</Form.Label>
-                    <Form.Control
-                        type="type"
-                        value={type}
-                        onChange={(e) => settype(e.target.value)}
-                    />
-                    </Form.Group>
+                    <h2>Add New Vehicle</h2>
+                    <div className="VehicleForm">
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group controlId="make">
+                                <Form.Label>Make</Form.Label>
+                                <select>
+                                    {makes.map((e) => {
+                                        <option 
+                                        key={e.id} 
+                                        value={e.id}>
+                                                {e.name}
+                                        </option>
+                                    })}
+                                </select>
+                            </Form.Group>
 
-                    <Form.Group controlId="newClass">
-                    <Form.Label>Class</Form.Label>
-                    <Form.Control
-                        type="newClass"
-                        value={newClass}
-                        onChange={(e) => setnewClass(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="type">
+                            <Form.Label>Type</Form.Label>
+                            <Form.Control
+                                type="type"
+                                value={type}
+                                onChange={(e) => settype(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newModel">
-                    <Form.Label>Model</Form.Label>
-                    <Form.Control
-                        type="newModel"
-                        value={newModel}
-                        onChange={(e) => setnewModel(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newClass">
+                            <Form.Label>Class</Form.Label>
+                            <Form.Control
+                                type="newClass"
+                                value={newClass}
+                                onChange={(e) => setnewClass(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newYear">
-                    <Form.Label>Year</Form.Label>
-                    <Form.Control
-                        type="newYear"
-                        value={newYear}
-                        onChange={(e) => setnewYear(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newModel">
+                            <Form.Label>Model</Form.Label>
+                            <Form.Control
+                                type="newModel"
+                                value={newModel}
+                                onChange={(e) => setnewModel(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newPrice">
-                    <Form.Label>Price</Form.Label>
-                    <Form.Control
-                        type="newPrice"
-                        value={newPrice}
-                        onChange={(e) => setnewPrice(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newYear">
+                            <Form.Label>Year</Form.Label>
+                            <Form.Control
+                                type="newYear"
+                                value={newYear}
+                                onChange={(e) => setnewYear(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newExtColor">
-                    <Form.Label>Exterior Color</Form.Label>
-                    <Form.Control
-                        type="newExtColor"
-                        value={newExtColor}
-                        onChange={(e) => setnewExtColor(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newPrice">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control
+                                type="newPrice"
+                                value={newPrice}
+                                onChange={(e) => setnewPrice(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newShortDescription">
-                    <Form.Label>Short Description</Form.Label>
-                    <Form.Control
-                        type="newShortDescription"
-                        value={newShortDescription}
-                        onChange={(e) => setnewShortDescription(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newExtColor">
+                            <Form.Label>Exterior Color</Form.Label>
+                            <Form.Control
+                                type="newExtColor"
+                                value={newExtColor}
+                                onChange={(e) => setnewExtColor(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newIntColor">
-                    <Form.Label>Interior Color (optional)</Form.Label>
-                    <Form.Control
-                        type="newIntColor"
-                        value={newIntColor}
-                        onChange={(e) => setnewIntColor(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newShortDescription">
+                            <Form.Label>Short Description</Form.Label>
+                            <Form.Control
+                                type="newShortDescription"
+                                value={newShortDescription}
+                                onChange={(e) => setnewShortDescription(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newEngine">
-                    <Form.Label>Engine (optional)</Form.Label>
-                    <Form.Control
-                        type="newEngine"
-                        value={newEngine}
-                        onChange={(e) => setnewEngine(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newIntColor">
+                            <Form.Label>Interior Color (optional)</Form.Label>
+                            <Form.Control
+                                type="newIntColor"
+                                value={newIntColor}
+                                onChange={(e) => setnewIntColor(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newTransmission">
-                    <Form.Label>Transmission (optional)</Form.Label>
-                    <Form.Control
-                        type="newTransmission"
-                        value={newTransmission}
-                        onChange={(e) => setnewTransmission(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newEngine">
+                            <Form.Label>Engine (optional)</Form.Label>
+                            <Form.Control
+                                type="newEngine"
+                                value={newEngine}
+                                onChange={(e) => setnewEngine(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newMilage">
-                    <Form.Label>Mileage (optional)</Form.Label>
-                    <Form.Control
-                        type="newMilage"
-                        value={newMilage}
-                        onChange={(e) => setnewMilage(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newTransmission">
+                            <Form.Label>Transmission (optional)</Form.Label>
+                            <Form.Control
+                                type="newTransmission"
+                                value={newTransmission}
+                                onChange={(e) => setnewTransmission(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newDescription">
-                    <Form.Label>Full Description (optional)</Form.Label>
-                    <Form.Control
-                        type="newDescription"
-                        value={newDescription}
-                        onChange={(e) => setnewDescription(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newMilage">
+                            <Form.Label>Mileage (optional)</Form.Label>
+                            <Form.Control
+                                type="newMilage"
+                                value={newMilage}
+                                onChange={(e) => setnewMilage(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Form.Group controlId="newImage">
-                    <Form.Label>Image (optional)</Form.Label>
-                    <Form.Control
-                        type="newImage"
-                        value={newImage}
-                        onChange={(e) => setnewImage(e.target.value)}
-                    />
-                    </Form.Group>
+                            <Form.Group controlId="newDescription">
+                            <Form.Label>Full Description (optional)</Form.Label>
+                            <Form.Control
+                                type="newDescription"
+                                value={newDescription}
+                                onChange={(e) => setnewDescription(e.target.value)}
+                            />
+                            </Form.Group>
 
-                    <Button className="" block type="submit" disabled={!validateForm()}>
-                        Add
-                    </Button>
-                </Form>
-            </div>
+                            <Button className="" block type="submit" disabled={!validateForm()}>
+                                Add
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+            } {/*end ternary statement */}
         </div>
+                        
     )
 }
