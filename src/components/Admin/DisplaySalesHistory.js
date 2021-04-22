79189a53cca76b48c9838aaf13f360d73ffd67c1
Taylor Cook <tecook2@mail.fhsu.edu>
2021-04-22 12:59:46 -0500

    Update DisplaySalesHistory.js

--- src/components/Admin/DisplaySalesHistory.js
+++ src/components/Admin/DisplaySalesHistory.js
@@ -1,333 +1,36 @@
-import React from 'react'
-import Table from 'react-bootstrap/Table'
+import React, { useState, useEffect } from "react";
+import 'regenerator-runtime/runtime';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function DisplaySalesHistory() {
     const url = `${window.location.protocol}//${window.location.hostname}/sales`;
-    const [deleted, setDeleted] = useState(0);
     const [data, setData] = useState(null);
-    const [deleteID, setDeleteID] = useState(null);
-    const [editID, setEditID] = useState(null);
-    const [userID, setUserID] = useState(null);
-    const [saleItems, setSaleItems] = useState(null);
-    const [status, setStatus] = useState(null);
-    const [date, setDate] = useState(null);
-    const [address, setAddress] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [editClicked, setEditClicked] = useState(false);
+    const [deleted, setDeleted] = useState(0);
+    const [reload, setReload] = useState(0);
 
-    function updateSubmit(event) {
-    }
+    //api fetch call for part list
+    useEffect(async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+        setLoading(false);
+    }, [deleted, reload]); //only rerender when deleted changes
+    //empty array for onMount only
 
-    function updateDelete(event) {
-    }
+    useEffect(async () => {
+        const response = await fetch(`${window.location.protocol}//${window.location.hostname}/sale_items`);
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+        setLoading(false);
+    }, [deleted, reload]);
     
-
     return (
-        <div className = "container-fluid">
-            <h2>Sales History</h2>
-
-            <div className="tablediv">
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>Vehicle ID</th>
-                        <th>Quantity</th>
-                        <th>Order Status</th>
-                        <th>Date</th>
-                        <th>Customer Address</th>
-                        <th>Update?</th>
-                        <th>Delete?</th>
-                    </tr>
-                </thead>
-                <tbody>                                        
-                    {/*foreach loop to display here*/}
-
-                    {/*below is filler to show table formatting when filled*/}
-                    <tr>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>Completed</td>
-                        <td>01/01/2021</td>
-                        <td>123 Main Street, Citytown KS, 55555</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-                    
-
-                    {/*Below this is filler for table only*/}
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <Form className="Update" onSubmit={updateSubmit}>
-                                <Button className="UpdateButton" block type="submit">
-                                    Update
-                                </Button>
-                            </Form>
-                        </td>
-                        <td>
-                            <Form className="Delete" onSubmit={updateDelete}>
-                                <Button className="DeleteButton" block type="submit">
-                                    Delete
-                                </Button>
-                            </Form>
-                        </td>
-                    </tr>
-
-
-                </tbody>
-            </Table>
-            </div>
+        <div>
+            Testing2
         </div>
     )
 }
