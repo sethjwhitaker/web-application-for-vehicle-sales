cdbc51dd3e35f1b558594d9f211adff381f8ef5f
Seth Whitaker <sethjwhitaker@yahoo.com>
2021-04-27 12:01:10 -0700

    Continue work on script

--- create_test_vehicles.py
+++ create_test_vehicles.py
@@ -1,10 +1,37 @@
 import requests
 import json
-with open("data/db.json") as file:
-    db = json.load(file)
-    data = db["cars"]
 
-    for d in data:
-        print(d)
-        #data incompatible at the moment
+url = "http://localhost:80/vehicles"
+with open("data/db.json") as file:
+    data = json.load(file)
+
+    #get makes classes types (add them manualy)
+
+    makes = []
+    classes = []
+    types = []
+
+
+
+    d = data[0]
+    print(d)
+
+    obj = {
+        "make_id": 1,
+        "type_id": 1,
+        "class_id": 1,
+        "model": d.model,
+        "year": d.year,
+        "price": d.price,
+        "exterior_color": d.exterior_color,
+        "interior_color": d.interior_color,
+        "engine": str(d.engine) + " L",
+        "transmission": "6-speed automatic",
+        "mileage": "30,000",
+        "short_description": d.short_description,
+        "description": "Example full description"
+    }
+        
+    response = requests.post(url, data=obj)
+    print(response)
 
