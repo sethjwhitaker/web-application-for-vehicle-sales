acd9e01faad4c0114f34cbad5d39a374d312841a
BradyNeumann <bneumann97@gmail.com>
2021-04-28 06:33:31 -0500

    Header styling again

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import './Header.css';
+import './Header.css'
 
 class Header extends Component {
     render() {
@@ -8,24 +8,24 @@ class Header extends Component {
             <div>
                 <header>
                     <Carousel>
-                        <Carousel.Item>
-                            <img className="d-block w-100" src = {'img/car-header.jpg'} />
-                            <Carousel.Caption className="d-sm-block">
-                                <h4>All the cars you could ever want.</h4>
+                        <Carousel.Item style = {{'height': "300px", 'width':"100%"}}>
+                            <img style = {{'height' : "300px"}} className="d-block w-100" src = {'img/car-header.jpg'} />
+                            <Carousel.Caption>
+                                <h3>All the cars you could ever want.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
 
-                        <Carousel.Item>
-                            <img className="d-block w-100" src = {'img/parts-header.jpg'} />
-                            <Carousel.Caption className="d-sm-block">
-                                <h4>All the parts you will ever need.</h4>
+                        <Carousel.Item style = {{'height': "300px", 'width':"100%"}}>
+                            <img style = {{'height' : "300px"}} className="d-block w-100" src = {'img/parts-header.jpg'} />
+                            <Carousel.Caption>
+                                <h3>All the parts you will ever need.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
 
-                        <Carousel.Item>
-                            <img className="d-block w-100" src = {'img/logo-header.png'} />
-                            <Carousel.Caption className="d-sm-block">
-                                <h4>All in one convenient online store.</h4>
+                        <Carousel.Item style = {{'height': "300px", 'width':"100%"}}>
+                            <img style = {{'height' : "300px"}} className="d-block w-100" src = {'img/logo-header.png'} />
+                            <Carousel.Caption>
+                                <h3>All in one convenient online store.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
