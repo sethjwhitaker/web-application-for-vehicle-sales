c01009a773e72d62574188bd38d7ac414d041975
BradyNeumann <bneumann97@gmail.com>
2021-04-28 05:17:44 -0500

    Carousel testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,21 +8,21 @@ class Header extends Component {
                 <header>
                     <Carousel>
                         <Carousel.Item style = {{'height': "325px", 'width':"100%"}}>
-                            <img style = {{'height' : "325px"}} className="d-block w-100" src = {'img/car-header.jpg'} />
+                            <img className="d-block w-100" src = {'img/car-header.jpg'} />
                             <Carousel.Caption>
                                 <h3>All the cars you could ever want.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
 
                         <Carousel.Item style = {{'height': "325px", 'width':"100%"}}>
-                            <img style = {{'height' : "325px"}} className="d-block w-100" src = {'img/parts-header.jpg'} />
+                            <img className="d-block w-100" src = {'img/parts-header.jpg'} />
                             <Carousel.Caption>
                                 <h3>All the parts you will ever need.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
 
                         <Carousel.Item style = {{'height': "325px", 'width':"100%"}}>
-                            <img style = {{'height' : "325px"}} className="d-block w-100" src = {'img/logo-header.png'} />
+                            <img className="d-block w-100" src = {'img/logo-header.png'} />
                             <Carousel.Caption>
                                 <h3>All in one convenient online store.</h3>
                             </Carousel.Caption>
