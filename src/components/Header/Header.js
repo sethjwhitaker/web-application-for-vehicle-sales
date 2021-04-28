fc3db62d0a378da2b7f337d404b4ef1bca1e9c9b
BradyNeumann <bneumann97@gmail.com>
2021-04-28 07:02:10 -0500

    Header Testing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,24 +8,24 @@ class Header extends Component {
             <div>
                 <header>
                     <Carousel>
-                        <Carousel.Item style = {{'height': "300px", 'width':"100%"}}>
+                        <Carousel.Item>
                             <img className="d-block w-100" src = {'img/car-header.jpg'} />
                             <Carousel.Caption>
                                 <h3 className="caption">All the cars you could ever want.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
 
-                        <Carousel.Item style = {{'height': "300px", 'width':"100%"}}>
+                        <Carousel.Item>
                             <img className="d-block w-100" src = {'img/parts-header.jpg'} />
-                            <Carousel.Caption className="caption">
-                                <h3>All the parts you will ever need.</h3>
+                            <Carousel.Caption>
+                                <h3 className="caption">All the parts you will ever need.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
 
-                        <Carousel.Item style = {{'height': "300px", 'width':"100%"}}>
+                        <Carousel.Item>
                             <img className="d-block w-100" src = {'img/logo-header.png'} />
-                            <Carousel.Caption className="caption">
-                                <h3>All in one convenient online store.</h3>
+                            <Carousel.Caption>
+                                <h3 className="caption">All in one convenient online store.</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
